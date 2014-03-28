56e97c5b9e56e797c37336788b603172890016ba
ionut.palade@twin.at <ionut.palade@twin.at@3eed900e-225e-adf5-8660-b956461ec5ba>
2014-03-28 18:51:35 +0000

    file "open" implementiert

--- trunk/file-copy/Palade.Ionut-Huber.Joerg/ph_copy.cc
+++ trunk/file-copy/Palade.Ionut-Huber.Joerg/ph_copy.cc
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <fcntl.h>
+#include <stdio.h>
 
 int main(int argc, char** argv)
 {
-
+	if(argc != 3){
+		printf("Error! User 2 arguments!\n");
+		return 1;
+	}
+	
+	int fo = open(argv[1], O_RDONLY);
+	printf("FileOpen: %i\n",fo);
+	
+	return 0;
 }
