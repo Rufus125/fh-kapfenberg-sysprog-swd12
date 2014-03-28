af32fc1c0fd7f9abe923d102effdeb7fb83df7d1
joerg86 <joerg86@gmail.com>
2014-03-28 19:37:14 +0000

    read file

--- file-copy/Palade.Ionut-Huber.Joerg/ph_copy.cc
+++ file-copy/Palade.Ionut-Huber.Joerg/ph_copy.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fcntl.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(int argc, char** argv)
 {
@@ -11,6 +13,32 @@ int main(int argc, char** argv)
 	
 	int fo = open(argv[1], O_RDONLY);
 	printf("FileOpen: %i\n",fo);
+	
+	char buf[1024] = "";
+	ssize_t rd = 0;
+	
+	while (true ) {
+		rd= read(fo, buf, sizeof(*buf));
+		
+		//check for read errors
+		if (rd < 0 )
+		{
+			printf("%s", buf);
+			break;
+		}
+		
+		//check for end of file
+		if (rd == 0)
+		{
+			printf("\nread end buf value %s", buf);
+			break;		
+		}
+		
+		//try to write to file
+
+		
+		printf("%s", buf);
+	}
 	
 	return 0;
 }
