e041e03f643f1bebc07907bf31b5bdbd05ba4109
totbojan <totbojan@3eed900e-225e-adf5-8660-b956461ec5ba>
2014-04-02 11:17:46 +0000

    added the cp-clone

---
+++ file-copy/Bojan.Tot/copy_tot.cpp
@@ -0,0 +1,76 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <iostream>
+
+#define BUF_SIZ 4096
+
+int writeFile(int source, int dest, ssize_t infile, ssize_t outfile, char buffer[]);
+
+int main(int argc, char** argv) {
+
+	char* inpath = argv[1];
+	char* outpath = argv[2];
+
+	if (argc < 3) {
+		std::cerr << "Please enter the source and the destination file."
+				<< std::endl;
+		return (EINVAL);
+	}
+
+	if (argc > 3) {
+		std::cerr << "Two arguments required." << std::endl;
+		return (EINVAL);
+	}
+
+	int fo = open(inpath, O_RDONLY);
+
+	if (!fo) {
+		std::cerr << "Error opening file!" << std::endl;
+		return (ENOENT);
+	}
+
+	int fw = open(outpath, O_WRONLY | O_CREAT | O_TRUNC);
+
+	if(!fw){
+		std::cerr << "Error creating output file!" << std::endl;
+		return (EIO);
+	}
+
+	char buffer[BUF_SIZ];
+	ssize_t infile = read(fo, buffer, BUF_SIZ);
+	ssize_t outfile = 0;
+	writeFile(fo, fw, infile, outfile, buffer);
+
+	close(fo);
+	close(fw);
+
+	return 0;
+}
+
+int copyFile(int source, int dest, ssize_t infile, ssize_t outfile, char buffer[]){
+
+	while (infile > 0){
+
+		outfile = write(dest, buffer, infile);
+		if (outfile == -1) {
+			std::cerr << "Could not write file" << std::endl;
+			return (EIO);
+		}
+		infile = read(source, buffer, BUF_SIZ);
+		if (infile == -1) {
+			std::cerr << "Could not read file" << std::endl;
+			return (EIO);
+		}
+
+	}
+	return 0;
+}
+
+
+
+
+
