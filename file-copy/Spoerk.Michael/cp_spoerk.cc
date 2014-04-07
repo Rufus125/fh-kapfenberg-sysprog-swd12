4e7b8cd8dc868da8764cb03c153c4184b305dbff
jaymz7683 <jaymz7683@gmail.com>
2014-04-07 14:45:32 +0000

    cp_spoerk refactored

--- file-copy/Spoerk.Michael/cp_spoerk.cc
+++ file-copy/Spoerk.Michael/cp_spoerk.cc
@@ -4,108 +4,59 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
-#define MAX_BUFFER_SIZE 4096
+#define BUFFER_SIZE 4096
 
 int main(int argc, char** argv)
 {
 	if(argc != 3)
 	{
-		std::cerr << "wrong number of arguments" << std::endl;
+		std::cerr << "wrong number of arguments. cp_spoerk SOURCE DESTINATION" << std::endl;
 		return 1;
-	}
-	else
-	{
-		std::cout << "copy: " << argv[1] << " to " << argv[2] << ": ";
 	}
 
 	int fd_source, fd_dest, bytesRead;
 	char *buffer = NULL;
-	unsigned long fileSize = 0; 
-	unsigned long partSize = 0;
 	
-	fd_source = open(argv[1], O_RDONLY);	
+	fd_source = open(argv[1], O_RDONLY | O_EXCL);	
 	if(fd_source < 0)
 	{
-		perror(argv[1]);
+		perror("could not open source file");
 		return 1;
 	}
 	
-	remove(argv[2]);
-
-	fd_dest = open(argv[2], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	fd_dest = open(argv[2], O_WRONLY | O_CREAT | O_EXCL, 0644);
 	if(fd_dest < 0)
 	{
-		perror(argv[2]);
+		perror("could not open destination file");
 		return 1;
 	}
 
-	fileSize = lseek(fd_source, 0, SEEK_END);
-	lseek(fd_source, 0, SEEK_SET);
-
-	std::cout << fileSize << " bytes!" << std::endl;
-
-	if(fileSize <= MAX_BUFFER_SIZE)
+	buffer = (char*) malloc(sizeof(char) * BUFFER_SIZE);	
+	if(buffer == NULL)
 	{
-		buffer = (char*) malloc(sizeof(char) * fileSize);
-		read(fd_source, buffer, fileSize);
-		
-		if(write(fd_dest, buffer, fileSize) <= 0)
+		std::cerr << "could not allocate buffer memory" << std::endl;
+		return 1;
+	}
+
+	while(bytesRead = read(fd_source, buffer, BUFFER_SIZE))
+	{
+		if(bytesRead <= 0)
 		{
-			perror("write()");
+			perror("could not read source file");
 			return 1;
 		}
 
-		if(buffer != NULL)
+		if(!write(fd_dest, buffer, bytesRead))
 		{
-			free(buffer);
-			buffer = NULL;
+			perror("could not write destination file");
+			return 1;
 		}
 	}
-	else
+
+	if(buffer != NULL)
 	{
-		buffer = (char*) malloc(sizeof(char) * MAX_BUFFER_SIZE);
-		partSize = MAX_BUFFER_SIZE;
-		
-		while(read(fd_source, buffer, partSize))
-		{
-			if(write(fd_dest, buffer, partSize) <= 0)
-			{
-				perror("write()");
-				return 1;
-			}
-
-			if(buffer != NULL)
-			{
-				free(buffer);
-				buffer = NULL;
-			}
-
-			partSize = fileSize - lseek(fd_source, 0, SEEK_CUR);
-
-			if (partSize <= MAX_BUFFER_SIZE)
-			{
-				buffer = (char*) malloc(sizeof(char) * partSize);
-			}
-			else if(partSize > MAX_BUFFER_SIZE)
-			{
-				buffer = (char*) malloc(sizeof(char) * MAX_BUFFER_SIZE);
-				partSize = MAX_BUFFER_SIZE;
-			}
-		}
-
-		if(buffer != NULL)
-		{
-			free(buffer);
-			buffer = NULL;
-		}
-	}
-	
-	if(lseek(fd_source, 0 , SEEK_CUR) != fileSize)
-	{
-		std::cout << "Error. Could not read whole file" << std::endl;
+		free(buffer);
 	}
 	
 	close(fd_source);
