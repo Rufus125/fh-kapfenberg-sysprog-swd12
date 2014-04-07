0ffdeba53abe4a3c4b09095a15249657d11a621a
joerg86 <joerg86@gmail.com>
2014-04-07 17:25:26 +0000

    tods implemented

--- file-copy/Palade.Ionut-Huber.Joerg/ph_copy.cc
+++ file-copy/Palade.Ionut-Huber.Joerg/ph_copy.cc
@@ -3,45 +3,35 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 using namespace std;
 
-const int err_arguments = 1;
-const int err_openfile = 2;
-const int err_writefile = 3;
-const int err_readfile = 4;
-
-//TODO:
-//immer exit code 1 retur geben bei fehler
-//perror implementieren
-//"Wenn SOURCE existiert und DESTINATION nicht extistiert kopiert es...
-// O_EXCL in den open Command implementieren
-// permission 0666
 
 int main(int argc, char** argv)
 {
 	if(argc != 3){
-		cout << "Error! Use 2 arguments!\n";
-		return err_arguments;
+		cerr << "Error! Use 2 arguments!\n";
+		return EXIT_FAILURE;
 	}
 	
 	int fos = open(argv[1], O_RDONLY);
 	if(fos < 0){
-		cerr << "Error! Couldn't open source file: " << argv[1] << endl;
-		return err_openfile;
+		perror ("Error while opening source file");
+		return EXIT_FAILURE;
 	}
 	
-	int fod = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, S_IRUSR | S_IXUSR);
+	int fod = open(argv[2], O_EXCL | O_CREAT | O_WRONLY, 0644);
 	if(fod < 0){
-		cerr << "Error! Couldn't open destination file: " << argv[2] << endl;
-		return err_openfile;
+		perror ("Error while opening destination file");
+		return EXIT_FAILURE;
 	}
 	
 	
 	char buf[1024] = "";
 	ssize_t rd = 0;
 	int fw;
-	int err_while_read_write = 0;
+	bool err_while_read_write = false;
 	
 	//read from source file and write do dest file
 	while (true ) {
@@ -52,8 +42,8 @@ int main(int argc, char** argv)
 		//check for read errors
 		if (rd < 0 )
 		{
-			cerr << "Error! Couldn't read from source file: " << argv[1] << endl;
-			err_while_read_write = err_readfile;
+			perror ("The following read error occurred");
+			err_while_read_write = true;
 			break;
 		}
 			
@@ -63,8 +53,8 @@ int main(int argc, char** argv)
 		//check for file write error
 		if ( fw < 0 )
 		{
-			cerr << "Error! Couldn't not write to destination file: " << argv[2] << endl;
-			err_while_read_write = err_writefile;
+			perror ("The following write error occurred");
+			err_while_read_write = true;
 			break;		
 		}
 		
@@ -86,12 +76,10 @@ int main(int argc, char** argv)
 	
 	if (err_while_read_write != 0)
 	{
-		printf("error while executing %d\n", err_while_read_write);
-		return err_while_read_write;
+		return EXIT_FAILURE;;
 	}
 	else
 	{
-		printf("file copy succed\n");
 		return 0;
 	}
 	
