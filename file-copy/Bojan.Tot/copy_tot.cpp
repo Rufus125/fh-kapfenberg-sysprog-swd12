90798daa573f48b96f71925990e193997a2c3195
totbojan <totbojan@3eed900e-225e-adf5-8660-b956461ec5ba>
2014-04-09 17:55:54 +0000

    completed (hopefully) the task

--- file-copy/Bojan.Tot/copy_tot.cpp
+++ file-copy/Bojan.Tot/copy_tot.cpp
@@ -1,5 +1,12 @@
+/*
+ * Tot Bojan
+ * Date: 08.04.14
+ * Descr: cp-clone
+ */
+
 #include <fcntl.h>
 #include <stdio.h>
+#include<stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -8,41 +15,49 @@
 
 #define BUF_SIZ 4096
 
-int copyFile(int source, int dest, ssize_t infile, ssize_t outfile, char buffer[]);
+int copyFile(int source, int dest, ssize_t infile, ssize_t outfile,
+		char buffer[]);
 
 int main(int argc, char** argv) {
 
 	char* inpath = argv[1];
 	char* outpath = argv[2];
+	int fw;
+	int fo;
+	int permission = (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	char buffer[BUF_SIZ];
+	ssize_t infile;
+	ssize_t outfile = 0;
 
 	if (argc < 3) {
-		std::cerr << "Please enter the source and the destination file."
-				<< std::endl;
-		return (EINVAL);
+		perror("Please enter the source and the destination file.");
+		exit(EXIT_FAILURE);
 	}
 
 	if (argc > 3) {
-		std::cerr << "Two arguments required." << std::endl;
-		return (EINVAL);
+		perror("Two arguments required.");
+		exit(EXIT_FAILURE);
 	}
 
-	int fo = open(inpath, O_RDONLY);
-
-	if (!fo) {
-		std::cerr << "Error opening file!" << std::endl;
-		return (ENOENT);
+	if ((fo = open(inpath, O_RDONLY)) < 0) {
+		perror("Error opening file!");
+		close(fo);
+		exit(EXIT_FAILURE);
 	}
 
-	int fw = open(outpath, O_WRONLY | O_CREAT | O_TRUNC);
+	if ((fw = open(outpath, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, permission))
+			< 0) {
 
-	if(!fw){
-		std::cerr << "Error creating output file!" << std::endl;
-		return (EIO);
+		if (errno == EEXIST) {
+			perror("Error ");
+		} else {
+			perror("Error creating output file!");
+		}
+		close(fw);
+		exit(EXIT_FAILURE);
 	}
 
-	char buffer[BUF_SIZ];
-	ssize_t infile = read(fo, buffer, BUF_SIZ);
-	ssize_t outfile = 0;
+	infile = read(fo, buffer, BUF_SIZ);
 	copyFile(fo, fw, infile, outfile, buffer);
 
 	close(fo);
@@ -51,26 +66,24 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-int copyFile(int source, int dest, ssize_t infile, ssize_t outfile, char buffer[]){
+int copyFile(int source, int dest, ssize_t infile, ssize_t outfile,
+		char buffer[]) {
 
-	while (infile > 0){
+	while (infile > 0) {
+
+		infile = read(source, buffer, BUF_SIZ);
+		if (infile == -1) {
+			perror("Could not read file");
+			exit(EXIT_FAILURE);
+		}
 
 		outfile = write(dest, buffer, infile);
 		if (outfile == -1) {
-			std::cerr << "Could not write file" << std::endl;
-			return (EIO);
-		}
-		infile = read(source, buffer, BUF_SIZ);
-		if (infile == -1) {
-			std::cerr << "Could not read file" << std::endl;
-			return (EIO);
+			perror("Could not write file");
+			exit(EXIT_FAILURE);
 		}
 
 	}
 	return 0;
 }
-
-
-
-
 
