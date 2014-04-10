ac9b81b2f0faec6112b718b188c6024efc7f2196
achim.kotremba@gmail.com <achim.kotremba@gmail.com@3eed900e-225e-adf5-8660-b956461ec5ba>
2014-04-10 17:28:18 +0000

    made some changes

--- trunk/file-copy/Kotremba.Achim-Windhaber.Thomas/copy.cc
+++ trunk/file-copy/Kotremba.Achim-Windhaber.Thomas/copy.cc
@@ -7,48 +7,48 @@
 
 using namespace std;
 
+#define BUFFER_SIZE 4096
+
 int main(int argc, char** argv)
 {
-    if (argc != 3)
-    {
-    	cout << "You need to define the source and target" << endl;
-    	return 1;
-    }
+  if (argc != 3)
+  {
+     cerr << "You need to define the source and target" << endl;
+     return EXIT_FAILURE;
+  }
 
-    cout << "source: " << argv[1] << " Target: " << argv[2] << endl;
+  cout << "source: " << argv[1] << " Target: " << argv[2] << endl;
 	
 	int source = open(argv[1], O_RDONLY);
-	int target = open(argv[2], O_WRONLY | O_CREAT | O_EXCL, S_IRUSR);
-	
-	if (!source || source < 0)
+  if (!source || source < 0)
 	{
 		cerr << "Can't find file " << argv[1] << endl;
 		close(source);
-		close(target);
-		return 1;
+		return EXIT_FAILURE;
 	}
-	
-	if (!target || target < 0)
+  
+  int target = open(argv[2], O_WRONLY | O_CREAT | O_EXCL, 0666);
+	if (target < 0)
 	{
-		cerr << "Can't find file " << argv[2] << endl;
+		cerr << "Targetfile " << argv[2] << " already exists." << endl;
 		close(source);
 		close(target);
-		return 1;
+		return EXIT_FAILURE;
 	}
 	
-	int *buffer[2048];
-	int bytes = read(source, buffer, 2048);
+	int *buffer[BUFFER_SIZE];
+	int bytes = read(source, buffer, BUFFER_SIZE);
 	
 	if (bytes != write(target, buffer, bytes))
 	{
 		cerr << "Unable to write data to file " << argv[2] << endl;
 		close(source);
 		close(target);
-		return 1;
+		return EXIT_FAILURE;
 	}
 	
 	cout << "File successfully copied to " << argv[2] << endl;
 	close(source);
 	close(target);
-	return 0;
+	return EXIT_SUCCESS;
 }
