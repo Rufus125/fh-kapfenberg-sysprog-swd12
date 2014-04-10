6c6f164a20502e76534fa4096bca9cc0208136ef
doktordos <doktordos@hotmail.com>
2014-04-10 18:24:41 +0000

    Changes

--- file-copy/Frech.Daniel-Hoesele.Daniel/copy.cc
+++ file-copy/Frech.Daniel-Hoesele.Daniel/copy.cc
@@ -10,8 +10,10 @@
 
 using namespace std;
 
-const int SIZE = 4096;
+const int BUFFER_SIZE = 4096;
 const int FAILURE = 1;
+const int SUCCESS = 1;
+
 
 int main(int argc, char** argv)
 {
@@ -20,38 +22,40 @@ int main(int argc, char** argv)
         exit(FAILURE);
     }
     
+    #ifdef DEBUG
     cout << "Source: " << argv[1] << endl;
     cout << "Destination: " << argv[2] << endl;
+    #endif
     
     int source = open(argv[1], O_RDONLY);
     int dest = open (argv[2], O_WRONLY | O_CREAT | O_EXCL,  0666);
     
     if (source == -1 || !source) {
-		cerr << "Failed to open file: " << argv[1] << endl;
-		cerr << strerror(errno) << endl;
+		perror("Failed to open source file");
 		close(source);
 		exit(FAILURE);
 	}
 	else if (dest == -1 || !dest) {
-		cerr << "Failed to open file: " << argv[2] << endl;
-		cerr << strerror(errno) << endl;
+		perror("Failed to open destination file");
 		close(source);
 		close(dest);
 		exit(FAILURE);
 	}
 	else {
-		int *buffer[SIZE];
-		int bytes = read(source, buffer, SIZE);
-		if(bytes != write(dest, buffer, bytes)) {
-			cerr << "Could not write to file: " << argv[2] << endl;
-			cerr << strerror(errno) << endl;
-			close(source);
-			close(dest);
-			exit(FAILURE);
+		int *buffer[BUFFER_SIZE];
+		int bytes = 0;
+		
+		while((bytes = read(source, buffer, BUFFER_SIZE)) != 0) {
+			if(bytes != write(dest, buffer, bytes)) {
+				perror("Could not write to destination file");
+				close(source);
+				close(dest);
+				exit(FAILURE);
+			}
 		}
 	}
-	cout << "Data successfully written to: " << argv[2] << endl;
+	cerr << "Data successfully written to: " << argv[2] << endl;
 	close(source);
 	close(dest);
-	return 0;
+	return SUCCESS;
 }
