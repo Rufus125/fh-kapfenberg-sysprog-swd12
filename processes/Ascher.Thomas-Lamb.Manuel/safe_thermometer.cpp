74e576d926539769870c544966711b64076d7049
thomas.ascher.swd12 <Thomas.Ascher.SWD12@gmail.com>
2014-04-11 13:47:54 +0000

    basic impl

--- processes/Ascher.Thomas-Lamb.Manuel/safe_thermometer.cpp
+++ processes/Ascher.Thomas-Lamb.Manuel/safe_thermometer.cpp
@@ -1,5 +1,12 @@
 #include "safe_thermometer.h"
 
+SafeThermometer::SafeThermometer(Thermometer* unsafe_thermometer)
+	:_unsafe_thermometer(unsafe_thermometer)
+{
+
+}
+
 double SafeThermometer::get_temperature() const
 {
+	return _unsafe_thermometer->get_temperature();
 }
