23f37ea35a36fb119615366e075528ab4392dcf1
platzer.f@gmail.com <platzer.f@gmail.com@3eed900e-225e-adf5-8660-b956461ec5ba>
2014-04-12 10:18:38 +0000

    Platzer Franz & Schlagholz Georg: finished thermo

--- trunk/processes/Schlagholz.Georg-Platzer.Franz/safe_thermo.cpp
+++ trunk/processes/Schlagholz.Georg-Platzer.Franz/safe_thermo.cpp
@@ -10,6 +10,7 @@ SafeThermometer::~SafeThermometer() {
 	delete unstableThermometer;
 }
 
+
 double SafeThermometer::get_temperature() const {
 	int pipe_fd[2];
 	int err = pipe(pipe_fd);
@@ -17,17 +18,46 @@ double SafeThermometer::get_temperature() const {
 		perror("pipe");
 		return 1;
 	}
-	int child = fork();
+
+	pid_t child = fork();
 	if (child == -1) {
 		perror("fork");
 		return 1;
 	} else if (child == 0) {
-		//child
-		//alarm
+		//child, close parent pipe (reading)
+		close(pipe_fd[0]);
+
+		alarm(timeout);
+
+		double temperature = unstableThermometer->get_temperature();
+		char tmp[DOUBLE_SIZE];
+		snprintf(tmp,DOUBLE_SIZE, "%f", temperature);
+
+		ssize_t written = write(pipe_fd[1], tmp, DOUBLE_SIZE); 
+		if(written < 0){
+			perror("write temperature");
+		}
+
+		close(pipe_fd[1]);
+
 		exit(0);
 	} else {
-		//pause
-		waitpid(child);
+		// parent, close child pipe (writing)
+		close(pipe_fd[1]);
+		int status;
+		waitpid(child, &status, 0);
+
+		if(status == 0){
+			char readbuf[DOUBLE_SIZE];
+            ssize_t nread = read(pipe_fd[0], readbuf, DOUBLE_SIZE);
+            if (nread > 0) {
+                return atof(readbuf);
+            }
+            else {
+                throw ThermometerException("Could not read temperature from unstable thermometer!");
+            }		
+		}else{
+			throw ThermometerException("Unstable thermometer!");
+		}
 	}
-	return 0;
 }
