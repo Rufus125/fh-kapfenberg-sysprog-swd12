939da7c3974f89b83dd31514b48c19d5eafa43f4
joerg86@gmail.com <joerg86@gmail.com@3eed900e-225e-adf5-8660-b956461ec5ba>
2014-04-14 19:54:34 +0000

    basic functions implemnted

--- trunk/processes/Palade.Ionut-Huber.Joerg/safe_test.cpp
+++ trunk/processes/Palade.Ionut-Huber.Joerg/safe_test.cpp
@@ -6,8 +6,29 @@
 #include <iostream>
 using namespace std;
 
+void ExecThermometer(Thermometer* thermometerToExec);
+
 int main()
 {
-		SafeThermometer th(new FixedThermometer(24));
+	ExecThermometer(new FixedThermometer(21));
+		
+	timespec hang_time = {4,10};
+	ExecThermometer(new HangThermometer(hang_time, 36.5));
+	
+	ExecThermometer(new RandomThermometer());
+	
+	ExecThermometer(new CrashThermometer());
+}
+
+void ExecThermometer(Thermometer* thermometerToExec)
+{
+	try
+	{
+		SafeThermometer th(thermometerToExec);
 		cout << th.get_temperature() << endl;
+	}
+	catch (ThermometerException& ex)
+	{
+		cerr << ex.what() << endl;
+	}	
 }
