ea82de619e44672a1ecf8cd41c2e374221e620d8
rufus.pwner <Rufus.Pwner@gmail.com>
2014-04-30 12:34:15 +0000

    Only temperature output to cout. Everything else deleted or written to cerr. Much funnier revision is 176

--- processes/Schlagholz.Georg-Platzer.Franz/safe_test.cpp
+++ processes/Schlagholz.Georg-Platzer.Franz/safe_test.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main(void){
 
-	cout << "--- FixedThermometer" << endl;
+	//cout << "--- FixedThermometer" << endl;
 	try
 	{
 		SafeThermometer st(new FixedThermometer (36.5));
@@ -19,50 +19,42 @@ int main(void){
 		cerr << ex.what() << endl;
 	}
 
-	cout << "--- RandomThermometer" << endl;
+	//cout << "--- RandomThermometer" << endl;
 	try
 	{
 		SafeThermometer st(new RandomThermometer());
-    	cout << "one random temperature: " << st.get_temperature() << endl;
-        cout << "another random temperature: " << st.get_temperature() << endl;
-        cout << "yet another random temperature: " << st.get_temperature() << endl;
+		cout << st.get_temperature() << endl;
+		cout << st.get_temperature() << endl;
+		cout << st.get_temperature() << endl;
 	}
 	catch(ThermometerException ex)
 	{
 		cerr << ex.what() << endl;
 	}
 
-	cout << "--- HangThermometer" << endl;
+	//cout << "--- HangThermometer" << endl;
 	try
 	{
 		timespec hang_time = {5,0};
 		SafeThermometer st(new HangThermometer(hang_time, 13.37));
-        cout << "Temperature will come after " << hang_time.tv_sec << " seconds "
-            "and " << hang_time.tv_nsec << " nanoseconds ..." << endl;
-        cout << "Well, here we go: " << st.get_temperature() << endl;
+        cout << st.get_temperature() << endl;
 
 		timespec hang_too_long = {8,0};
 		SafeThermometer stTimeoutIncreased(new HangThermometer(hang_too_long, 47.11), 10); // timeout bigger than longer hang time
-        cout << "Temperature will come after " << hang_too_long.tv_sec << " seconds "
-            "and " << hang_too_long.tv_nsec << " nanoseconds ... " << endl;
-		cout << "Timeout set to 8!" << endl;
-        cout << "Well, here we go: " << stTimeoutIncreased.get_temperature() << endl;
+        cout << stTimeoutIncreased.get_temperature() << endl;
 		
 		SafeThermometer stTimeoutReached(new HangThermometer(hang_too_long, 47.11));
-        cout << "Temperature will come after " << hang_too_long.tv_sec << " seconds "
-            "and " << hang_too_long.tv_nsec << " nanoseconds ... But now the twist: our timeout is set to 6!" << endl;
-        cout << "Well, here we go: " << stTimeoutReached.get_temperature() << endl;
+        cout << stTimeoutReached.get_temperature() << endl;
 	}
 	catch(ThermometerException ex)
 	{
 		cerr << ex.what() << endl;
 	}
 
-	cout << "--- CrashThermometer" << endl;
+	//cout << "--- CrashThermometer" << endl;
 	try
 	{
 		SafeThermometer st(new CrashThermometer());
-    	cout << "And now for something completely different: " << flush;
 		cout << st.get_temperature() << endl;
 	}
 	catch(ThermometerException ex)
