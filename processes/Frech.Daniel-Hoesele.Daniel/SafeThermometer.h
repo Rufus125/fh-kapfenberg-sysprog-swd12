c9c7cb9e71e5dcead58215fd87c029fb9736d841
doktordos <doktordos@hotmail.com>
2014-05-15 17:29:16 +0000

    EX2

--- processes/Frech.Daniel-Hoesele.Daniel/SafeThermometer.h
+++ processes/Frech.Daniel-Hoesele.Daniel/SafeThermometer.h
@@ -1,1 +1,23 @@
 // Daniel Hoesele - Daniel Frech - SWD12
+
+#include "../sensors/thermometer.h"
+#include <unistd.h>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+class SafeThermometer : public Thermometer {
+	public:
+		SafeThermometer(Thermometer* unstableThermometer, unsigned int timeout = 10);
+		virtual ~SafeThermometer();
+		virtual double get_temperature() const;
+
+	private:
+		Thermometer* unstableThermometer;
+		unsigned int timeout;
+		static const int BUFFER_SIZE = 64;
+};
--- processes/Frech.Daniel-Hoesele.Daniel/SafeThermometerTest.cpp
+++ processes/Frech.Daniel-Hoesele.Daniel/SafeThermometerTest.cpp
@@ -1,1 +1,68 @@
 // Daniel Hoesele - Daniel Frech - SWD12
+
+#include "SafeThermometer.h"
+#include "../sensors/fixed_thermometer.h"
+#include "../sensors/hang_thermometer.h"
+#include "../sensors/random_thermometer.h"
+#include "../sensors/crash_thermometer.h"
+
+using namespace std;
+
+int main()
+{
+	//SafeFixedThermoTest:
+    cout << "--- FixedThermometer" << endl;
+    try
+    {
+		SafeThermometer th(new FixedThermometer (36.5));
+    	cout << th.get_temperature() << endl;
+    }
+    catch(ThermometerException ex)
+	{
+		cerr << ex.what() << endl;
+	}
+
+	//SafeRandomThermoTest:
+    cout << "--- RandomThermometer" << endl;
+    try
+    {
+        SafeThermometer th(new RandomThermometer());
+        cout << "one random temperature: " << th.get_temperature() << endl;
+        cout << "another random temperature: " << th.get_temperature() << endl;
+        cout << "yet another random temperature: " << th.get_temperature() << endl;
+    }
+    catch(ThermometerException ex)
+	{
+		cerr << ex.what() << endl;
+	}
+
+    //SafeHangThermoTest:
+    cout << "--- HangThermometer" << endl;
+    try
+    {
+        timespec hang_time = {5,0};
+        SafeThermometer th(new HangThermometer(hang_time, 36.5));
+        cout << "Temperature will come after " << hang_time.tv_sec << " seconds "
+            "and " << hang_time.tv_nsec << " nanoseconds ..." << endl;
+        cout << "Well, here we go: " << th.get_temperature() << endl;
+    }
+    catch(ThermometerException ex)
+	{
+		cerr << ex.what() << endl;
+	}
+
+    //SafeCrashThermoTest:
+    cout << "--- CrashThermometer" << endl;
+    try
+    {
+        SafeThermometer th(new CrashThermometer());
+        cout << "And now something completely different: " << flush;
+        cout << th.get_temperature() << endl;
+    }
+	catch(ThermometerException ex)
+	{
+		cerr << ex.what() << endl;
+	}
+
+    return 0;
+}
