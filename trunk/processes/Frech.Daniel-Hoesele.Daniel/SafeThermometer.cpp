1cde4e661b79db5c44284db6fee621e6ff6cfa9b
doktordos@hotmail.com <doktordos@hotmail.com@3eed900e-225e-adf5-8660-b956461ec5ba>
2014-05-15 17:29:16 +0000

    EX2

--- trunk/processes/Frech.Daniel-Hoesele.Daniel/SafeThermometer.cpp
+++ trunk/processes/Frech.Daniel-Hoesele.Daniel/SafeThermometer.cpp
@@ -1,61 +1,67 @@
 // Daniel Hoesele - Daniel Frech - SWD12
 
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <string.h>
+#include "SafeThermometer.h"
 
 using namespace std;
 
-const int BUFFER_SIZE = 4096;
-const int FAILURE = 1;
-const int SUCCESS = 1;
+SafeThermometer::SafeThermometer(Thermometer* unstableThermometer,
+		unsigned int timeout):
+		unstableThermometer(unstableThermometer), timeout(timeout){}
 
+SafeThermometer::~SafeThermometer() {
+	delete unstableThermometer;
+}
 
-int main(int argc, char** argv)
-{
-    if (argc != 3) {
-        cerr << "Program needs exactly 2 arguments" << endl;
-        exit(FAILURE);
-    }
-    
-    #ifdef DEBUG
-    cout << "Source: " << argv[1] << endl;
-    cout << "Destination: " << argv[2] << endl;
-    #endif
-    
-    int source = open(argv[1], O_RDONLY);
-    int dest = open (argv[2], O_WRONLY | O_CREAT | O_EXCL,  0666);
-    
-    if (source == -1 || !source) {
-		perror("Failed to open source file");
-		close(source);
-		exit(FAILURE);
+double SafeThermometer::get_temperature() const {
+
+	int pipe_fd[2];
+	int err = pipe(pipe_fd);
+
+	if (err == -1) {
+		perror("pipe");
+		abort();
 	}
-	else if (dest == -1 || !dest) {
-		perror("Failed to open destination file");
-		close(source);
-		close(dest);
-		exit(FAILURE);
-	}
-	else {
-		int *buffer[BUFFER_SIZE];
-		int bytes = 0;
-		
-		while((bytes = read(source, buffer, BUFFER_SIZE)) != 0) {
-			if(bytes != write(dest, buffer, bytes)) {
-				perror("Could not write to destination file");
-				close(source);
-				close(dest);
-				exit(FAILURE);
-			}
+	
+	pid_t pid = fork();
+
+	if (pid == -1) {
+		perror("fork");
+		abort();
+
+	} else if (pid == 0) {
+		// child. close parent's end.
+		close(pipe_fd[0]);
+		alarm(timeout);
+
+		double temperature = unstableThermometer->get_temperature();
+		char buffer[BUFFER_SIZE];
+		snprintf(buffer, BUFFER_SIZE, "%f", temperature);
+		ssize_t written= write(pipe_fd[1], buffer, BUFFER_SIZE);
+
+		if(written < 0){
+			perror("write");
+		}
+		// parent. close child's end.
+		close(pipe_fd[1]);
+		exit(0);
+
+	} else {
+		close(pipe_fd[1]);
+		int status;
+		waitpid(pid, &status, 0);
+
+		if(status == 0) {
+			char buffer[BUFFER_SIZE];
+            ssize_t nread = read(pipe_fd[0], buffer, BUFFER_SIZE);
+
+            if (nread > 0) {
+                return atof(buffer);
+            }
+            else {
+                throw ThermometerException("failed to get temperature");
+            }		
+		} else {
+			throw ThermometerException("unstable thermometer exception thrown");
 		}
 	}
-	cerr << "Data successfully written to: " << argv[2] << endl;
-	close(source);
-	close(dest);
-	return SUCCESS;
 }
