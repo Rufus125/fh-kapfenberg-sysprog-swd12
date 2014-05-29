72fe4549c87650e092ddd73b3d391509f209229b
haberl.george@gmail.com <haberl.george@gmail.com@3eed900e-225e-adf5-8660-b956461ec5ba>
2014-05-29 06:53:32 +0000

    block colors now changable

--- trunk/project/Haberl.Hans-Georg/tetris_blocks.h
+++ trunk/project/Haberl.Hans-Georg/tetris_blocks.h
@@ -1,5 +1,6 @@
 #include <SDL/SDL.h>
 
+
 #define DARK_RED        {139,0,0, 0}
 #define LIGHT_GRAY      {211,211,211, 0}
 #define DARK_MAGENTA    {139,0,139, 0}
@@ -8,6 +9,14 @@
 #define DARK_BROWN      {92,64,51, 0}
 #define DARK_CYAN       {0,139,139, 0}
 
+static SDL_Color color_I = DARK_RED;
+static SDL_Color color_J = LIGHT_GRAY;
+static SDL_Color color_L = DARK_MAGENTA;
+static SDL_Color color_O = DARK_BLUE;
+static SDL_Color color_S = DARK_GREEN;
+static SDL_Color color_T = DARK_BROWN;
+static SDL_Color color_Z = DARK_CYAN;
+
 enum Tetrimino_Type {
     I, J, L, O, S, T, Z
 };
@@ -15,7 +24,7 @@ enum Tetrimino_Type {
 typedef struct {
     int x;
     int y;
-    SDL_Color color;    
+    SDL_Color* color;    
 } Block;
 
 typedef struct {
