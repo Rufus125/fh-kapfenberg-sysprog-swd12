be9c0277b10b7fe5557604d9a9a31aef0f595f9d
at.fhj.swd12@gmail.com <at.fhj.swd12@gmail.com@3eed900e-225e-adf5-8660-b956461ec5ba>
2014-05-30 18:51:34 +0000

    Aufsplittung von Client und Server

---
+++ trunk/project/Marcher.Reitner.Siharath.Tot/AlarmServer/src/at/fhj/swd/RaspberryGPIO.java
@@ -0,0 +1,21 @@
+package at.fhj.swd;
+
+public class RaspberryGPIO implements GPIO {
+
+	public RaspberryGPIO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void on() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void off() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
