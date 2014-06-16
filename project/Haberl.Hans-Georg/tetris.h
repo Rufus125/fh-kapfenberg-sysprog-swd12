d4ea18e3627a8a4edd5c6537a31b5a21245d07f7
haberl.george <haberl.george@gmail.com>
2014-06-16 10:36:09 +0000

    forgot h file

---
+++ project/Haberl.Hans-Georg/tetris.h
@@ -0,0 +1,21 @@
+#ifndef TETRIS
+#define TETRIS
+#include "tetris_colors.h"
+
+
+static const int BOARD_BLOCKS_X = 10;
+static const int BOARD_BLOCKS_Y = 20;
+static const int BOARD_OFFSET_X = 230;
+static const int BOARD_OFFSET_Y = 50+60;
+static const int BLOCK_DIMENSIONS = 15;
+
+static const int TETRIMINO_BLOCK_NUM = 4;
+
+static const int* BOARD_COLOR = &BLACK;
+
+
+
+
+
+
+#endif
