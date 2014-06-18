418cf9e8e1ec5f9fd96db0f7393c574654417eea
rufus.pwner <Rufus.Pwner@gmail.com>
2014-06-18 19:50:47 +0000

    It is working now getServerInfo updated to work with server ServerInfo updated documentation localhost.pem is a file that cointains private key and certificate server contains the server which needs ServerInfo

--- project/Schlagholz.Georg-Mayer.Michael-Platzer.Franz/getServerInfo.py
+++ project/Schlagholz.Georg-Mayer.Michael-Platzer.Franz/getServerInfo.py
@@ -1,36 +1,45 @@
 #!/usr/bin/python3
+# coding=UTF-8
 # Author: Michael Mayer
 # Teammembers:Georg Schlagholz, Franz Platzer, Michael Mayer
 import os
+import sys, re, urllib.request, datetime
+import time
 
 def call_server(server):
-    pass
+   response= urllib.request.urlopen('https://'+server).read().decode('utf8')
+   response = response.split('\t')
+   response[0] = response[0].replace("Hostname: ", "")
+   response[1] = response[1].replace("Temperature: ", "")
+   response[1] = response[1].strip()
+   return response
 
 def function():
   #get serverlist
-  servers = []
-  chidren = []
-  r, w = os.pipe() # these are file descriptors
+  servers = ["192.168.178.89:4443", "192.168.178.93:4443"]
+  children = []
   for server in servers:
+    r, w = os.pipe() # these are file descriptors
     wpid = os.fork()
     if wpid==0:
       """wpid is 0 means We are in Child_process"""
       children.append(wpid)
-      call_server(server)
       os.close(r)
       w = os.fdopen(w, 'w')
-      print "child: writing"
-      w.write("here's some text from the child")
+      entry = call_server(server)
+      entry = str(entry)
+      w.write(entry)
       w.close()
       os._exit(0)
     elif wpid > 0:
       os.close(w) # use os.close() to close a file descriptor
       r = os.fdopen(r) # turn r into a file object
-      print "parent: reading"
       txt = r.read()
+      print(txt)
       for child in children:
         #wait for finish, then output
         os.waitpid(child, 0)
+        os._exit(0)
           
 if __name__ == "__main__":
   function()
