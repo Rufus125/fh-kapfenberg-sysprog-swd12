f915b0ddbd64f459bd6d1f2214e13997f95b16fc
rufus.pwner <Rufus.Pwner@gmail.com>
2014-06-18 20:55:41 +0000

    Some refactoring deleted unused imports correct indentation added print for ip adresse and port

--- project/Schlagholz.Georg-Mayer.Michael-Platzer.Franz/getServerInfo.py
+++ project/Schlagholz.Georg-Mayer.Michael-Platzer.Franz/getServerInfo.py
@@ -3,46 +3,46 @@
 # Author: Michael Mayer
 # Teammembers:Georg Schlagholz, Franz Platzer, Michael Mayer
 import os
-import sys, re, urllib.request, datetime
-import time
+import urllib.request
 
 def call_server(server):
-   response= urllib.request.urlopen('https://'+server).read().decode('utf8')
-   response = response.split('\t')
-   response[0] = response[0].replace("Hostname: ", "")
-   response[1] = response[1].replace("Temperature: ", "")
-   response[1] = response[1].strip()
-   return response
+    """ server: a URL to the server IP:Port
+        returns one entry: a tuple: hostname (string) and temperature in celsius (string) xXX.X_°C
+     """
+    response = urllib.request.urlopen('https://' + server).read().decode('utf8')  #get response from server
+    response = response.split('\t')  #split by tab
+    response[0] = response[0].replace("Hostname: ", "")  # remove unuseful info
+    response[1] = response[1].replace("Temperature: ", "")
+    response[1] = response[1].strip()  #delete newline
+    return response
 
-def function():
-  #get serverlist
-  servers = ["192.168.178.89:4443", "192.168.178.93:4443"]
-  children = []
-  for server in servers:
-    r, w = os.pipe() # these are file descriptors
-    wpid = os.fork()
-    if wpid==0:
-      """wpid is 0 means We are in Child_process"""
-      children.append(wpid)
-      os.close(r)
-      w = os.fdopen(w, 'w')
-      entry = call_server(server)
-      entry = str(entry)
-      w.write(entry)
-      w.close()
-      os._exit(0)
-    elif wpid > 0:
-      os.close(w) # use os.close() to close a file descriptor
-      r = os.fdopen(r) # turn r into a file object
-      txt = r.read()
-      print(txt)
-      for child in children:
-        #wait for finish, then output
-        os.waitpid(child, 0)
-        os._exit(0)
-          
+def call_servers():
+    """ Iterates over each item in servers array and gets the server info"""
+    #get serverlist
+    servers = ["192.168.178.89:4443"]
+    children = []
+    for server in servers:
+        r, w = os.pipe()  # these are file descriptors
+        wpid = os.fork()
+        if wpid == 0:
+            """wpid is 0 means We are in Child_process"""
+            children.append(wpid)
+            os.close(r)  #close the read pipe
+            w = os.fdopen(w, 'w')  # turn w into a file object
+            entry = call_server(server)  #call server to get server infos
+            entry = str(entry)  #convert to a str to be able to write it to pipe
+            w.write(entry)  #write to pipe
+            w.close()  #close pipe file descriptor
+            os._exit(0)  #exit child process
+        elif wpid > 0:
+            os.close(w)  # use os.close() to close a file descriptor
+            r = os.fdopen(r)  # turn r into a file object
+            txt = r.read()  # read the input from child
+            print(txt)
+            for child in children:
+                #wait for finish, then output
+                os.waitpid(child, 0)
+                os._exit(0)
+
 if __name__ == "__main__":
-  function()
-  #fork for each server
-  #get results after specific time
-  #output result
+    call_servers()
--- project/Schlagholz.Georg-Mayer.Michael-Platzer.Franz/server.py
+++ project/Schlagholz.Georg-Mayer.Michael-Platzer.Franz/server.py
@@ -1,4 +1,4 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 # coding=UTF-8
 # Author: Georg Schlagholz
 # Teammembers:Georg Schlagholz, Franz Platzer, Michael Mayer 
@@ -7,36 +7,39 @@ import http.server
 import ssl
 import socketserver
 import socket
-#This class will handles any incoming request from
+#This class will handle any incoming request from
 #the browser
 
 
 # openssl req -new -x509 -newkey rsa:2048 -out cert.csr -nodes
 # cat privkey.pem cert.csr >> localhost.pem
 
-#TODO: built in a print for IP adresse
-class myHandler(http.server.SimpleHTTPRequestHandler):
-  #Handler for the GET requests
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type','text/plain; charset=utf-8')
-    self.end_headers()
-    # Send the response
-    hostname,temp = ServerInfo.ServerInfo().getInfos()
-    response = "Hostname: "+hostname+"\tTemperature: "+str(temp)+" °C\n"
-    self.wfile.write(bytes(response, 'UTF-8'))
-    return
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    #Handler for the GET requests
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain; charset=utf-8') #UTF-8 for degree sign
+        self.end_headers()
+        # Send the response
+        hostname, temp = ServerInfo.ServerInfo().getInfos()
+        response = "Hostname: " + hostname + "\tTemperature: " + str(temp) + " °C\n"
+        self.wfile.write(bytes(response, 'UTF-8'))
+        return
+
 
 try:
-  ipaddr = [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
-  server_address = (ipaddr, 4443)
-  httpd = http.server.HTTPServer(server_address, myHandler)
-  httpd.socket = ssl.wrap_socket(httpd.socket,
-                               server_side=True,
-                               certfile='localhost.pem',
-                               ssl_version=ssl.PROTOCOL_TLSv1)
-  httpd.serve_forever()
+    ipaddr = [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in
+              [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1] #get ipaddr
+    port = 4443
+    server_address = (ipaddr, port)
+    httpd = http.server.HTTPServer(server_address, MyHandler)
+    httpd.socket = ssl.wrap_socket(httpd.socket,
+                                   server_side=True,
+                                   certfile='localhost.pem',
+                                   ssl_version=ssl.PROTOCOL_TLSv1)
+    print("Starting server: "+ipaddr+":"+str(port))
+    httpd.serve_forever()
 
 except KeyboardInterrupt:
-  httpd.socket.close()
-  print(' received, shutting down the web server')
+    httpd.socket.close()
+    print(' received, shutting down the web server')
