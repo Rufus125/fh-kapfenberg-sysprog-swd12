cbf7f0a5c5b5e374f124e1ca0f2322f51f3c8864
rufus.pwner@gmail.com <rufus.pwner@gmail.com@3eed900e-225e-adf5-8660-b956461ec5ba>
2014-06-25 19:42:09 +0000

    Server.py = output to stderr ServerInfo = now should work with more linuxes

--- trunk/project/Schlagholz.Georg-Mayer.Michael-Platzer.Franz/ServerInfo.py
+++ trunk/project/Schlagholz.Georg-Mayer.Michael-Platzer.Franz/ServerInfo.py
@@ -2,13 +2,31 @@
 # coding=UTF-8
 # Author: Georg Schlagholz
 # Teammembers:Georg Schlagholz, Franz Platzer, Michael Mayer 
+import os
+
 class ServerInfo: 
     def getInfos(self):
       """returns hostname and temperature in celsius of the computer as a tuple"""
-      temp = int(open("/sys/class/thermal/thermal_zone0/temp", "r").read()) #get temp
-      temp /= 1000 #because value is in minicelsius
-      hostname = str(open("/etc/hostname", "r").read()).rstrip('\n') #get hostname
-      return (hostname,temp)
+      p = '/sys/class/thermal/thermal_zone0/temp'
+      if not os.path.isfile(p):
+        p = '/sys/bus/platform/devices/coretemp.0/temp1_input'
+        if not os.path.isfile(p):
+          p = '/sys/class/hwmon/hwmon2/temp1_input'
+          if not os.path.isfile(p):
+            p = '/sys/class/hwmon/hwmon1/temp1_input'
+            if not os.path.isfile(p):
+              p = '/sys/class/hwmon/hwmon0/temp1_input'
+              if not os.path.isfile(p):
+                p = '/sys/class/thermal/thermal_zone1/temp'
+                if not os.path.isfile(p):
+                  p = '/sys/class/thermal/thermal_zone3/temp'
+      if os.path.isfile(p):
+        temp = int(open(p, "r").read()) #get temp
+        temp /= 1000 #because value is in minicelsius
+        hostname = str(open("/etc/hostname", "r").read()).rstrip('\n') #get hostname
+        return (hostname,temp)
+      else:
+        raise Exception("Couldn't find temp files")
 if __name__ == "__main__":
   si = ServerInfo()
   hostname,temp = si.getInfos()
